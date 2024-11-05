72fbc3536bb2507850e473f6e24e07feda2eb2f2
qiuxan <qiuxan@gmail.com>
2024-11-05 13:42:51 +1100

    setup controller and config program.cs

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers;
+//localhost:xxxx/api/books
+[Route("api/[Controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    private readonly List<Book> _bookList;
+
+    public BooksController()
+    {
+        _bookList = new List<Book>
+        {
+            new Book { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald" },
+            new Book { Id = 2, Title = "Hellow world", Author = "Ian Q" },
+            new Book { Id = 3, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald" }
+        };
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Book>> GetBooks() 
+    {
+        return Ok(_bookList);
+    }
+}
--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -5,8 +5,12 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+
         var app = builder.Build();
 
+        app.MapControllers();
         app.MapGet("/", () => "Hello World!");
 
         app.Run();
